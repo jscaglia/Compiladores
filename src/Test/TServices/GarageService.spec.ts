db9535474bf8a66a24190b4defcda40cc07917a0
federico <fedeforciniti66@gmail.com>
2021-05-06 21:07:35 -0300

    testGarageservice

--- src/Test/TServices/GarageService.spec.ts
+++ src/Test/TServices/GarageService.spec.ts
@@ -1,14 +1,48 @@
 
 import { assert } from 'chai';
+import { PisoGarage } from '../../Infraestructure/Class/PisoGarage';
 import { GarageService } from '../../Services/GarageService';
+import { Auto } from '../../Infraestructure/Class/Vehiculos/Auto';
 
 describe('Test Class GarageService: ', () => {
+
 
     describe('Test Garage Service constructor: ', () => {
         it("Expect Garage Service Constructor: ", function () {
             var garageService = new GarageService();
             assert.equal(1, 1);
         });
+
+
+    });
+    describe('Test Garage Service GetPrimerLugarLibre: ', () => {
+        it("Expect Garage Service GetPrimerLugarLibre: ", function () {
+            var garageService = new GarageService();
+            var result = garageService.GetPrimerLugarLibre();
+            assert.equal(result?.Espacio,0);
+            assert.equal(result?.Piso,0);
+            console.log("primer lugar libre");
+        });
+
+
+    describe('Test Garage Service EntrarVehiculo: ', () => {
+        it("Expect Garage Service EntrarVehiculo: ", function () {
+            var garageService = new GarageService();
+            var result = garageService.EntrarVehiculo(new Auto,1,2);
+            assert.equal(result?.Espacio, 2);
+            assert.equal(result?.Piso, 1);
+            console.log("Ingreso nuevo vehiculo");
+        });
     });
 
+    describe('Test Garage Service QuitarVehiculo: ', () => {
+        it("Expect Garage Service QuitarVehiculo: ", function () {
+            var garageService = new GarageService();
+            var result = garageService.QuitarVehiculo(1,2);
+            assert.equal(result, 0);
+            console.log("vehiculo quitado");
+        });
+
+    });
+    });
 });
